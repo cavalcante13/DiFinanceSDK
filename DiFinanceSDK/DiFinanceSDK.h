73befb9a11b6b38ff57444b0673650f7ae4e82bb
Diego Cavalcante <cavalcante13.santos@gmail.com>
2023-03-23 15:57:31 -0300

    Initial Commit

---
+++ DiFinanceSDK/DiFinanceSDK.h
@@ -0,0 +1,18 @@
+//
+//  DiFinanceSDK.h
+//  DiFinanceSDK
+//
+//  Created by Diego on 23/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DiFinanceSDK.
+FOUNDATION_EXPORT double DiFinanceSDKVersionNumber;
+
+//! Project version string for DiFinanceSDK.
+FOUNDATION_EXPORT const unsigned char DiFinanceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiFinanceSDK/PublicHeader.h>
+
+
